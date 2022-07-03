b0e3a83e99b6a03a0e6309cf4d3710e95c5dbfce
MRCrBr <89889043+MRCrBr@users.noreply.github.com>
2022-07-03 17:01:46 -0400

    rutas creadas en views url, ingreando img en index

--- BlackHole/views.py
+++ BlackHole/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     return render (request,"index.html")
+
+def login(request):
+    return render (request,"login.html")
+
+def QuieneSomos(request):
+    return render (request,"QuieneSomos.html")
+
+def tienda(request):
+    return render (request,"tienda.html")
